06c956ec9fdd6df03c7067aa1bfa10e8cb4b7846
Kai Wang <540202901@qq.com>
2019-03-22 15:57:46 +0800

    添加Header和Tabbar

--- src/app.vue
+++ src/app.vue
@@ -1,6 +1,33 @@
 <template>
-    <div>
-        <h1>这时 app  组件</h1>
+    <div class="app-container">
+        <!-- 顶部 Header 区域-->
+        <mt-header fixed title="Webpack+Vue商城项目练习"></mt-header>
+
+
+
+        <!-- 中间的 路由 router-view 区域-->
+
+
+        <!-- 底部 Tabbar 区域-->
+        <nav class="mui-bar mui-bar-tab">
+            <a class="mui-tab-item mui-active" href="#tabbar">
+                <span class="mui-icon mui-icon-home"></span>
+                <span class="mui-tab-label">首页</span>
+            </a>
+            <a class="mui-tab-item" href="#tabbar-with-chat">
+                <span class="mui-icon mui-icon-email"><span class="mui-badge">9</span></span>
+                <span class="mui-tab-label">消息</span>
+            </a>
+            <a class="mui-tab-item" href="#tabbar-with-contact">
+                <span class="mui-icon mui-icon-contact"></span>
+                <span class="mui-tab-label">通讯录</span>
+            </a>
+            <a class="mui-tab-item" href="#tabbar-with-map">
+                <span class="mui-icon mui-icon-gear"></span>
+                <span class="mui-tab-label">设置</span>
+            </a>
+        </nav>
+        <h1>123</h1>
     </div>
 </template>
 
@@ -9,5 +36,7 @@
 </script>
 
 <style lang="scss" scoped>
-
+    .app-container{
+        margin-top:40px;
+    }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,2 +1,18 @@
 //入口文件
-console.log('ok')
+//=============引入框架包=======================
+import Vue from 'vue';
+import './lib/mui/dist/css/mui.min.css'
+//=============按需导入Mint UI组件=======================
+import { Header } from 'mint-ui';
+
+Vue.component(Header.name, Header);//Header组件
+
+//=============引入自定义组件=======================
+import app from './app.vue'
+
+
+//=============Vue根实例=======================
+var vm = new Vue({
+    el:'#app',
+    render: c => c(app)
+});
